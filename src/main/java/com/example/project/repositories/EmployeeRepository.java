9eaf32aaac7e34be1e8bb0ca953d433f1b794580
Trolom <142252316+Trolom@users.noreply.github.com>
2024-01-22 11:38:54 +0200

    Delete src directory

--- src/main/java/com/example/project/repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.example.project.entities.Employee;
-
-@Repository
-public interface EmployeeRepository extends MongoRepository<Employee,Integer>{}
--- src/main/java/com/example/project/repositories/ManufacturerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.example.project.entities.Manufacturer;
-
-@Repository
-public interface ManufacturerRepository extends MongoRepository<Manufacturer, Integer>{
-}
